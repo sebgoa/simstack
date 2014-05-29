0071e3295005d8c1ffe2acd3f91982c9af4f17bd
Bas Meijer <bas.meijer@me.com>
2014-05-29 12:13:16 +0200

    adding riak

--- ansible/roles/cloudstack/templates/bootstrap.sh
+++ ansible/roles/cloudstack/templates/bootstrap.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#setup DB
+/usr/local/maven/bin/mvn -Pdeveloper -pl developer -Ddeploydb
+/usr/local/maven/bin/mvn -Pdeveloper -pl developer -Ddeploydb-simulator
+
 #start mgt server
-nohup mvn -pl client jetty:run -Dsimulator &> /tmp/cloudstack.out &
+nohup /usr/local/maven/bin/mvn -pl client jetty:run -Dsimulator &> /tmp/cloudstack.out &
 echo "Starting management server on http://localhost:8080/client"
 
 #cfg datacenter
 #dirt wait to let the mgt server start
 sleep 30
-python ./tools/marvin/marvin/deployDataCenter.py -i setup/dev/basic.cfg
+/usr/local/bin/python2.7 ./tools/marvin/marvin/deployDataCenter.py -i setup/dev/basic.cfg
 
 #run awsapi from source
-nohup mvn -Pawsapi -pl :cloud-awsapi jetty:run &> /tmp/awsapi.out &
+nohup /usr/local/maven/bin/mvn -Pawsapi -pl :cloud-awsapi jetty:run &> /tmp/awsapi.out &
 echo "Starting AWSAPI interface on port 7080 use the awsapi.py script to test"
--- ansible/roles/cloudstack/templates/marvin.sh
+++ ansible/roles/cloudstack/templates/marvin.sh
@@ -4,6 +4,4 @@ cd /opt/cloudstack
 /usr/local/bin/virtualenv-2.7 marvin-dir
 source marvin-dir/bin/activate
 /usr/local/maven/bin/mvn -Pdeveloper,awsapi -Dsimulator -DskipTests clean install
-#setup DB
-/usr/local/maven/bin/mvn -Pdeveloper -pl developer -Ddeploydb
-/usr/local/maven/bin/mvn -Pdeveloper -pl developer -Ddeploydb-simulator
+
