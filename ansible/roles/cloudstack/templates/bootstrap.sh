f943cb3d54cfa9069908c13b54748073b24423ae
Bas Meijer <bas.meijer@me.com>
2014-11-12 22:09:20 +0100

    skip AWSAPI in bootstrap

--- ansible/roles/cloudstack/templates/bootstrap.sh
+++ ansible/roles/cloudstack/templates/bootstrap.sh
@@ -14,5 +14,5 @@ sleep 30
 /usr/local/bin/python2.7 ./tools/marvin/marvin/deployDataCenter.py -i setup/dev/basic.cfg
 
 #run awsapi from source
-nohup /usr/local/maven/bin/mvn -Pawsapi -pl :cloud-awsapi jetty:run &> /tmp/awsapi.out &
-echo "Starting AWSAPI interface on port 7080 use the awsapi.py script to test"
+#nohup /usr/local/maven/bin/mvn -Pawsapi -pl :cloud-awsapi jetty:run &> /tmp/awsapi.out &
+#echo "Starting AWSAPI interface on port 7080 use the awsapi.py script to test"
